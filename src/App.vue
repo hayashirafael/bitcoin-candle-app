8569ae70bf6353681e6b3af5fbcbc60d7fcb71e6
Rafael <rafaelhayashi8@gmail.com>
2023-05-11 16:04:41 -0300

    feat: criando componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
+<template>
+    <Header />        
+</template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import Header from './components/Header.vue'
 
 @Options({
-  components: {},
+  components: {
+    Header
+  },
 })
 export default class App extends Vue {}
 </script>
 
-<style></style>
+<style>
+body {
+    margin: 0
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="header">
+    <img :src="logo" alt="Bitcoin logo">
+    <span>Bitcoin Candle App</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator'
+import logo from '../assets/img/logo.png'
+
+@Component
+export default class Header extends Vue{
+    get logo() {
+        return logo
+    }
+}
+</script>
+
+<style>
+
+#header {
+    background-image: linear-gradient(to right, #4d05e8, #4871f7);
+    padding: 30px 10px;
+    margin-bottom: 10px;
+}
+
+#header * {
+    vertical-align: middle;
+}
+
+#header img {
+    width: 70px;
+}
+
+#header span {
+    color: white;
+    font-size: 200%;
+    margin-left: 20px;
+    text-shadow: 2px 2px #333;
+}
+
+</style>
