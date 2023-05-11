d439aa35b1b31d2682d920dfbfc8576996488604
Rafael <rafaelhayashi8@gmail.com>
2023-05-11 17:24:09 -0300

    feat: configurando grafico de candles

---
+++ src/components/CandleStickChart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <apexchart
+      type="candlestick"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-facing-decorator";
+import VueApexCharts from "vue3-apexcharts";
+
+@Component({
+  components: {
+    VueApexCharts,
+  },
+})
+@Component
+export default class CandleStickChart extends Vue {
+  @Prop()
+  candles = [];
+
+  chartOption = {
+    chart: {
+      type: "candlestick",
+      height: 350,
+    },
+    title: {
+      text: "Bitcoin last prices",
+      align: "center",
+    },
+    xaxis: {
+      type: "time",
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true,
+      },
+    },
+  };
+  
+  series = [{
+    data: this.candles
+  }]
+}
+</script>
+
+<style>
+</style>
